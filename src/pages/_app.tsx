a9ea41704b7278e0939bd21bd9a41bb7186f3ddc
Giuliane Eulalia Correa <57021894+GiulianeEC@users.noreply.github.com>
2021-12-11 21:04:19 -0300

    usando faunaDB

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
-import {AppProps} from 'next/app'
 import { Header } from '../components/header';
+import { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react";
+
 import '../styles/global.scss';
 
-function MyApp({ Component, pageProps }:AppProps) {
+function MyApp({ Component, pageProps}: AppProps ) {
   return (
-    <>
-    <Header /> 
-    <Component {...pageProps} />
-    </>
-  )  
+    <SessionProvider session={pageProps.session}>
+      <Header />
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
 }
 
 export default MyApp
